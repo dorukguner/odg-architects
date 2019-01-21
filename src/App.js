2fe808244a705a502d8de3bef84992ebb513acc2
dorukguner <stackmuch@gmail.com>
2019-01-21 23:34:31 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,97 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentMenuItem: "Profile",
+    }
+
+    this.handleMenuChange = this.handleMenuChange.bind(this);
   }
+
+  handleMenuChange(event) {
+    this.setState({ currentMenuItem: event.target.id });
+  }
+
+  render() {
+
+    return (
+      <Router>
+        <div>
+          <div id="menu-wrapper">
+            <Menu />
+          </div>
+
+          <div id="content">
+            <Route exact path="/" component={Profile} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+          </div>
+
+        </div>
+      </Router>
+    )
+  }
+};
+
+const Profile = () => (
+  <h2>Profile</h2>
+
+);
+
+const Contact = () => <h2>Contact Us</h2>;
+
+function PortfolioWhole({ match }) {
+  let incompleteList = incomplete.map((task) => this.createTasks(task, ""));
+  let completeList = complete.map((task) => this.createTasks(task, "completed"));
+  return (
+  "Projects"
+  <br>
+  <ul className="projectList">
+  <li className="project">
+    <Link to={`${match.url}/project1`}>Project 1</Link>
+  </li>
+  <li>
+    <Link to={`${match.url}/project2`}>Project 2</Link>
+  </li>
+</ul>
+  );
 }
+
+function Project({ match }) {
+    return (
+      "Single project" + match.params.id
+    )
+}
+
+const Portfolio = ({ match }) => (
+  <div>
+    <Route path={`${match.path}/:id`} component={Project} />
+    <Route
+      exact
+      path={match.path}
+      render={PortfolioWhole}
+    />
+  </div>
+);
+
+const Menu = () => (
+  <ul id="menu">
+    <li>
+      <Link to="/">Profile</Link>
+    </li>
+    <li>
+      <Link to="/portfolio">Portfolio</Link>
+    </li>
+    <li>
+      <Link to="/contact">Contact Us</Link>
+    </li>
+  </ul>
+);
 
 export default App;
